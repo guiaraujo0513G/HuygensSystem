1eea372c3f7ae721550e97c9cff669b2a49abece
matheussouza89 <matheus.souza89@fatec.sp.gov.br>
2020-03-05 16:44:06 -0300

    first

---
+++ Pages/JavaScript/popups.js
@@ -0,0 +1,56 @@
+function abrirDescritiva() {
+        document.getElementById('popupDescritiva').style.display='block'
+        document.getElementById('popupProbabilidadeEscolha').style.display='none'
+        document.getElementById('popupCorrelacao').style.display='none'
+        document.getElementById('popupProbabilidadeUniforme').style.display='none'
+        document.getElementById('popupProbabilidadeBinomial').style.display='none'
+        document.getElementById('popupProbabilidadeNormal').style.display='none'
+        document.getElementById('tabela-descritiva').style.display='none'
+        document.getElementById('medida-separatriz').style.display='none'
+
+}
+function abrirProbabilidadeEscolha() {
+        document.getElementById('popupDescritiva').style.display='none'
+        document.getElementById('popupProbabilidadeEscolha').style.display='block'
+        document.getElementById('popupCorrelacao').style.display='none'
+        document.getElementById('popupProbabilidadeUniforme').style.display='none'
+        document.getElementById('popupProbabilidadeBinomial').style.display='none'
+        document.getElementById('popupProbabilidadeNormal').style.display='none'
+        document.getElementById('tabela-descritiva').style.display='none'
+}
+function abrirCorrelacao() {
+        document.getElementById('popupDescritiva').style.display='none'
+        document.getElementById('popupProbabilidadeEscolha').style.display='none'
+        document.getElementById('popupCorrelacao').style.display='block'
+        document.getElementById('popupProbabilidadeUniforme').style.display='none'
+        document.getElementById('popupProbabilidadeBinomial').style.display='none'
+        document.getElementById('popupProbabilidadeNormal').style.display='none'
+        document.getElementById('tabela-descritiva').style.display='none'
+}
+function abrirProbabilidadeUniforme() {
+        document.getElementById('popupDescritiva').style.display='none'
+        document.getElementById('popupProbabilidadeEscolha').style.display='none'
+        document.getElementById('popupCorrelacao').style.display='none'
+        document.getElementById('popupProbabilidadeUniforme').style.display='block'
+        document.getElementById('popupProbabilidadeBinomial').style.display='none'
+        document.getElementById('popupProbabilidadeNormal').style.display='none'
+        document.getElementById('tabela-descritiva').style.display='none'
+}
+function abrirProbabilidadeBinomial() {
+        document.getElementById('popupDescritiva').style.display='none'
+        document.getElementById('popupProbabilidadeEscolha').style.display='none'
+        document.getElementById('popupCorrelacao').style.display='none'
+        document.getElementById('popupProbabilidadeUniforme').style.display='none'
+        document.getElementById('popupProbabilidadeBinomial').style.display='block'
+        document.getElementById('popupProbabilidadeNormal').style.display='none'
+        document.getElementById('tabela-descritiva').style.display='none'
+}
+function abrirProbabilidadeNormal() {
+        document.getElementById('popupDescritiva').style.display='none'
+        document.getElementById('popupProbabilidadeEscolha').style.display='none'
+        document.getElementById('popupCorrelacao').style.display='none'
+        document.getElementById('popupProbabilidadeUniforme').style.display='none'
+        document.getElementById('popupProbabilidadeBinomial').style.display='none'
+        document.getElementById('popupProbabilidadeNormal').style.display='block'
+        document.getElementById('tabela-descritiva').style.display='none'
+}
