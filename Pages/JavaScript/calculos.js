a4990444271575d5b978845a1c382dd4d5a331f7
matheussouza89 <matheus.souza89@fatec.sp.gov.br>
2020-03-06 15:15:43 -0300

    icon/scroll-behavior

--- Pages/JavaScript/calculos.js
+++ Pages/JavaScript/calculos.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
 })
 
 function abrirTbDescritiva() {
-
     // Caso amostra
     var nomeVariavel = $('input[name="nomeVariavel"]');
     var populacao = $('input[name="dadosInp"]')[0];
