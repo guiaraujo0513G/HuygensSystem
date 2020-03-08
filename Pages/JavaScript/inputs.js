9052198a18a2ea3f8cdbea78c0ccafbb9a470f07
matheussouza89 <matheus.souza89@fatec.sp.gov.br>
2020-03-08 00:23:29 -0300

    Alterações e ajustes de CSS

--- Pages/JavaScript/inputs.js
+++ Pages/JavaScript/inputs.js
@@ -1,3 +1,55 @@
-function abrirValor1() {
-    document.getElementById('menorQ-Probabilidade').style.display='block'
+function abrirComparacao() {
+            if(document.getElementById('comparacaoUniforme').value =='0') {
+                document.getElementById('menorQ-Probabilidade').style.display='none'
+                document.getElementById('inicial-Probabilidade').style.display='none'
+                document.getElementById('final-Probabilidade').style.display='none'
+                document.getElementById('valor-Probabilidade').style.display='none'
+            }
+            if(document.getElementById('comparacaoUniforme').value =='1'){
+
+                    document.getElementById('menorQ-Probabilidade').style.display='inline-block'
+                    document.getElementById('inicial-Probabilidade').style.display='none'
+                    document.getElementById('final-Probabilidade').style.display='none'
+                    document.getElementById('valor-Probabilidade').style.display='none'
+            }
+            if(document.getElementById('comparacaoUniforme').value =='2'){
+                    document.getElementById('menorQ-Probabilidade').style.display='none'
+                    document.getElementById('inicial-Probabilidade').style.display='inline-block'
+                    document.getElementById('final-Probabilidade').style.display='inline-block'
+                    document.getElementById('valor-Probabilidade').style.display='none'
+            }
+            if(document.getElementById('comparacaoUniforme').value =='3'){
+                    document.getElementById('menorQ-Probabilidade').style.display='none'
+                    document.getElementById('inicial-Probabilidade').style.display='none'
+                    document.getElementById('final-Probabilidade').style.display='none'
+                    document.getElementById('valor-Probabilidade').style.display='inline-block'
+            }
+
+}
+function abrirComparacaoN() {
+    if(document.getElementById('comparacaoNormal').value =='0') {
+        document.getElementById('menorQ-ProbabilidadeN').style.display='none'
+        document.getElementById('inicial-ProbabilidadeN').style.display='none'
+        document.getElementById('final-ProbabilidadeN').style.display='none'
+        document.getElementById('valor-ProbabilidadeN').style.display='none'
+    }
+    if(document.getElementById('comparacaoNormal').value =='1'){
+
+        document.getElementById('menorQ-ProbabilidadeN').style.display='inline-block'
+        document.getElementById('inicial-ProbabilidadeN').style.display='none'
+        document.getElementById('final-ProbabilidadeN').style.display='none'
+        document.getElementById('valor-ProbabilidadeN').style.display='none'
+    }
+    if(document.getElementById('comparacaoNormal').value =='2'){
+        document.getElementById('menorQ-ProbabilidadeN').style.display='none'
+        document.getElementById('inicial-ProbabilidadeN').style.display='inline-block'
+        document.getElementById('final-ProbabilidadeN').style.display='inline-block'
+        document.getElementById('valor-ProbabilidadeN').style.display='none'
+    }
+    if(document.getElementById('comparacaoNormal').value =='3'){
+        document.getElementById('menorQ-ProbabilidadeN').style.display='none'
+        document.getElementById('inicial-ProbabilidadeN').style.display='none'
+        document.getElementById('final-ProbabilidadeN').style.display='none'
+        document.getElementById('valor-ProbabilidadeN').style.display='inline-block'
+    }
 }
