6af28b91fed3395a19096b1d20198c6019fa1afb
guiaraujo0513G <54581401+guiaraujo0513G@users.noreply.github.com>
2020-03-10 22:34:15 -0300

    Update calculos.js

--- Pages/JavaScript/calculos.js
+++ Pages/JavaScript/calculos.js
@@ -29,9 +29,9 @@ function abrirTbDescritiva() {
     var FacDescritivaPercent = 0
     for ( var aux in agrupamentos) {
         if (typeof acm === 'undefined'){
-        var acm = `<tr><td>${aux}</td><td>${agrupamentos[aux]}</td><td>${((agrupamento[aux]*100)/populacaoArray.length).toFixed(2)}%</td><td>${FacDescritiva = FacDescritiva + agrupamentos[aux]}</td><td>${(FacDescritivaPercent = FacDescritivaPercent + ((agrupamento[aux]*100)/populacaoArray.length)).toFixed(2)%}</td></tr>`
+        var acm = `<tr><td>${aux}</td><td>${agrupamentos[aux]}</td><td>${((agrupamentos[aux]*100)/populacaoArray.length).toFixed(2)}%</td><td>${FacDescritiva = FacDescritiva + agrupamentos[aux]}</td><td>${(FacDescritivaPercent = FacDescritivaPercent + ((agrupamentos[aux]*100)/populacaoArray.length)).toFixed(2)}%</td></tr>`
         }else{
-            var acm = acm + `<tr><td>${aux}</td><td>${agrupamentos[aux]}</td><td>${((agrupamento[aux]*100)/populacaoArray.length).toFixed(2)}%</td><td>${FacDescritiva = FacDescritiva + agrupamentos[aux]}</td><td>${(FacDescritivaPercent = FacDescritivaPercent + ((agrupamento[aux]*100)/populacaoArray.length)).toFixed(2)%</td></tr>`
+            var acm = acm + `<tr><td>${aux}</td><td>${agrupamentos[aux]}</td><td>${((agrupamentos[aux]*100)/populacaoArray.length).toFixed(2)}%</td><td>${FacDescritiva = FacDescritiva + agrupamentos[aux]}</td><td>${(FacDescritivaPercent = FacDescritivaPercent + ((agrupamentos[aux]*100)/populacaoArray.length)).toFixed(2)}%</td></tr>`
         document.getElementById('frequencia-descritiva').innerHTML = acm
         }
         }
